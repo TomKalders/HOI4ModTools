cbb9dba6a64cab77ff522b01b1f9a5092eb86e5a
Tom Kalders <tom.kalders@hotmail.com>
2025-05-23 22:14:13 +0200

    feat(shine-gfx-generator): added program to generate shine files for given gfx file

---
+++ ShineGfxGenerator/GfxFile.cs
@@ -0,0 +1,19 @@
+﻿namespace ShineGfxGenerator;
+
+/// <summary>
+/// Model class for gfx file
+/// </summary>
+public class GfxFile
+{
+    public string FileName = "";
+    public readonly List<SpriteType> SpriteTypes = new();
+}
+
+/// <summary>
+/// Model for sprite type object from gfx file
+/// </summary>
+public class SpriteType
+{
+    public required string Name;
+    public required string TextureFile;
+}
---
+++ ShineGfxGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ShineGfxGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Check arguments for application
+            if (args.Length != 2)
+            {
+                ConsoleLogger.WriteErrorLine("Invalid usage");
+                ConsoleLogger.WriteErrorLine("Usage: ShineGfxGenerator.exe <source file> <output file>");
+                return;
+            }
+
+            //Read contents from gfx file
+            if (!GfxFileReader.TryToReadGfxFile(args[0], out var gfxFile, out var error))
+            {
+                ConsoleLogger.WriteErrorLine("Failed to read sprite types from gfx.");
+                ConsoleLogger.WriteErrorLine("Error:");
+                ConsoleLogger.WriteErrorLine(error);
+                return;
+            }
+
+            //Convert and write gfx file to shine gfx file
+            if (!GfxFileWriter.TryWriteGfxFileAsShineFile(gfxFile, args[1], out error))
+            {
+                ConsoleLogger.WriteErrorLine("Failed to write gfx shine file.");
+                ConsoleLogger.WriteErrorLine("Error:");
+                ConsoleLogger.WriteErrorLine(error);
+            }
+            
+            ConsoleLogger.WriteLine($"Successfully converted gfx file '{args[0]}' to shine gfx file '{args[1]}'", ConsoleColor.Green);
+        }
+    }
+}
